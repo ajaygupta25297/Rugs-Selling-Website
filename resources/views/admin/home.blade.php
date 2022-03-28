fc973b897d26ab6478264df96ae2531663625bb2
ajaygupta25297 <ajaygupta25297@gmail.com>
2022-03-28 22:25:21 +0530

    Third Commit

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,25 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    
+
+    @include(admin.css)
+  </head>
+  <body>
+    
+      <!-- partial -->
+      @include('admin.sidebar')
+      @include('admin.navbar')
+        <!-- partial -->
+      @include('admin.body')
+          <!-- partial -->
+      
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    
+    <!-- End custom js for this page -->
+    @include('admin.script')
+  </body>
+</html>
