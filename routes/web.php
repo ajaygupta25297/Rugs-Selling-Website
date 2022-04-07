b8e5359d6a85505658ed493155671c0c507c4cbb
ajaygupta25297 <ajaygupta25297@gmail.com>
2022-04-07 23:45:46 +0530

    fourth commit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\controllers\HomeController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,10 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+route::get('/redirect',[HomeController::class,'redirect']);
+
+
+route::get('/',[HomeController::class,'index']);
